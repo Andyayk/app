daa37f43918e4575bc51eb4e871f2ee4b18b8455
Andy Ang Yong Kiat <andy_ang94@hotmail.com>
2019-05-17 11:32:07 +0800

    - added template

--- scrapyapp/scrapyapp/items.py
+++ scrapyapp/scrapyapp/items.py
@@ -9,22 +9,6 @@ import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 from w3lib.html import remove_tags
 
-def remove_whitespace(value):
-	return value.strip()
-
-class JokeItem(scrapy.Item):
-	# define the fields for your item here like:
-	# name = scrapy.Field()
-	joke_text = scrapy.Field(
-		input_processor = MapCompose(remove_tags, remove_whitespace), #removing HTML tags and whitespace
-		output_processor = TakeFirst()
-	)
-
-class QuoteItem(scrapy.Item):
-	title = scrapy.Field()
-	author = scrapy.Field()
-	tag = scrapy.Field()
-
 class HRPolicyRelationItem(scrapy.Item):
 	relationship = scrapy.Field()
 	name = scrapy.Field()
--- scrapyapp/scrapyapp/pipelines.py
+++ scrapyapp/scrapyapp/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import mysql.connector, os
+import os
 from py2neo import Graph, Node, Relationship #neo4j
 from scrapyapp.items import HRPolicyRelationItem, DocumentItem
 
@@ -43,36 +43,3 @@ class ScrapyappPipeline(object):
 			file = open(path + os.sep + item['policyname'] + ".txt","w") #saving data into text file
 			file.write(item['document'])
 			file.close()
-
-	'''
-	def __init__(self):
-		self.create_connection()
-		self.create_table()
-
-	def create_connection(self):
-		self.conn = mysql.connector.connect(
-			host = 'localhost',
-			user = 'root',
-			passwd = '',
-			database = 'scrapyapp'
-		)
-		self.curr = self.conn.cursor()
-
-	def create_table(self):
-		self.curr.execute("""DROP TABLE IF EXISTS scrapyapp""")
-		self.curr.execute("""create table scrapyapp(
-						title text,
-						author text,
-						tag text
-						)""")
-
-	def store_db(self, item):
-		results = graph.run(query, {"limit": 500})
-
-		self.curr.execute("""insert into scrapyapp values (%s, %s, %s)""", (
-			item['title'][0],
-			item['author'][0],
-			item['tag'][0]
-		))
-		self.conn.commit()                      
-	'''
