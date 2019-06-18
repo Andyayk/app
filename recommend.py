8993db16c4071289a8e1ab7655c5dd0ea817dbbd
Andy Ang Yong Kiat <andy_ang94@hotmail.com>
2019-06-18 10:39:42 +0800

    - update recommend

--- recommend.py
+++ recommend.py
@@ -3,53 +3,40 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv('u.data', sep='\t', names=['user_id','item_id','rating','titmestamp'])
-
-print(df.head())
-
-movie_titles = pd.read_csv('Movie_Id_Titles')
-print(movie_titles.head())
-
-df = pd.merge(df, movie_titles, on='item_id')
+#read csv
+df = pd.read_csv('train.csv', sep=',')
 print(df.head())
 
 print(df.describe())
 
-ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
-print(ratings.head())
+#sort
+numsearch = pd.DataFrame(df.groupby('policy')['numsearch'].mean())
+print(numsearch.head())
 
-ratings['number_of_ratings'] = df.groupby('title')['rating'].count()
-print(ratings.head())
+#convert to dataframe matrix, policy as columns
+policy_matrix = df.pivot_table(index='username', columns='policy', values='numsearch')
+print(policy_matrix.head())
 
-movie_matrix = df.pivot_table(index='user_id', columns='title', values='rating')
-print(movie_matrix.head())
+#retrieve columns
+APP_user_search = policy_matrix['Academic pay policy']
+promotion_user_search = policy_matrix['Academic promotion']
+print(APP_user_search.head())
+print(promotion_user_search.head())
 
-print(ratings.sort_values('number_of_ratings', ascending=False).head(10))
+#find similarities
+similar_to_APP = policy_matrix.corrwith(APP_user_search)
+print(similar_to_APP.head())
 
-AFO_user_rating = movie_matrix['Air Force One (1997)']
-contact_user_rating = movie_matrix['Contact (1997)']
+#find similarities
+similar_to_promotion = policy_matrix.corrwith(promotion_user_search)
+print(similar_to_promotion.head())
 
-print(AFO_user_rating.head())
-print(contact_user_rating.head())
+#convert to dataframe
+corr_APP = pd.DataFrame(similar_to_APP, columns=['correlation'])
+corr_APP.dropna(inplace=True)
+print(corr_APP.head())
 
-similar_to_air_force_one = movie_matrix.corrwith(AFO_user_rating)
-print(similar_to_air_force_one.head())
-
-similar_to_contact = movie_matrix.corrwith(contact_user_rating)
-print(similar_to_contact.head())
-
-corr_AFO = pd.DataFrame(similar_to_air_force_one, columns=['correlation'])
-corr_AFO.dropna(inplace=True)
-print(corr_AFO.head())
-
-corr_contact = pd.DataFrame(similar_to_contact, columns=['Correlation'])
-corr_contact.dropna(inplace=True)
-print(corr_contact.head())
-
-corr_AFO = corr_AFO.join(ratings['number_of_ratings'])
-corr_contact = corr_contact.join(ratings['number_of_ratings'])
-
-print(corr_AFO[corr_AFO['number_of_ratings'] > 100].sort_values(by='correlation', ascending=False).head(10))
-
-print(corr_contact[corr_contact['number_of_ratings'] > 100].sort_values(by='Correlation', ascending=False).head(10))
-
+#convert to dataframe
+corr_promotion = pd.DataFrame(similar_to_promotion, columns=['Correlation'])
+corr_promotion.dropna(inplace=True)
+print(corr_promotion.head())
